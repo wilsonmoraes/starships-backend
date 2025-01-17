93940eeed89944379cad5fe53569bf2bf65b137b
Wilson Moraes dos Santos <wilsontads@gmail.com>
2025-01-17 08:21:00 -0300

    added wrapper's

--- app/routes.py
+++ app/routes.py
@@ -1,69 +1,12 @@
 import logging
 
-import requests
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask import Blueprint, request, jsonify
 
-from .auth import authenticate_user, login_user, logout_user, is_authenticated
 from .models.db.starships import Starship, Manufacturer, starship_manufacturer
 
 logger = logging.getLogger(__name__)
 
 app_routes = Blueprint("app_routes", __name__)
-
-
-@app_routes.route("/", methods=["GET", "POST"])
-def login():
-    import os
-    print("Template Folder:", os.path.join(os.getcwd(), "templates"))
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if authenticate_user(username, password):
-            login_user(username)
-            return redirect(url_for("app_routes.dashboard"))
-        return render_template("login.html", error="Invalid credentials")
-    return render_template("login.html")
-
-
-@app_routes.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for("app_routes.login"))
-
-
-@app_routes.route("/dashboard")
-def dashboard():
-    if not is_authenticated():
-        return redirect(url_for("app_routes.login"))
-
-    manufacturer_filter = request.args.get("manufacturer")
-
-    try:
-        # Fetch manufacturers
-        manufacturers_response = requests.get(url_for("app_routes.get_manufacturers", _external=True))
-        manufacturers_response.raise_for_status()
-        manufacturers = manufacturers_response.json()
-
-        # Fetch starships with optional manufacturer filter
-        starships_url = url_for("app_routes.get_starships", _external=True)
-        if manufacturer_filter:
-            starships_url = f"{starships_url}?manufacturer_id={manufacturer_filter}"
-
-        starships_response = requests.get(starships_url)
-        starships_response.raise_for_status()
-        detailed_starships = starships_response.json()
-
-    except requests.RequestException as e:
-        logger.error(f"Error fetching data from API: {e}")
-        manufacturers = []
-        detailed_starships = []
-
-    return render_template(
-        "dashboard.html",
-        starships=detailed_starships,
-        manufacturers=manufacturers,
-        selected_manufacturer=manufacturer_filter  # Passa o ID selecionado
-    )
 
 
 @app_routes.route("/api/manufacturers", methods=["GET"])
@@ -123,6 +66,6 @@ def get_starship_detail(starship_id):
         "created_at": starship.created_at,
         "edited_at": starship.edited_at,
         "url": starship.url,
-        "manufacturers": [m.manufacturer.name for m in starship.manufacturers],
+        "manufacturers": [m.name for m in starship.manufacturers],
     }
     return jsonify(result)
